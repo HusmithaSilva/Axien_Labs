79a7d728f251ebe479dae6ec5371d6ff35ccb11f
HusmithaSilva <husmitha.silva0@gmail.com>
2023-05-07 15:02:04 +0530

    refactored

--- Constants/nodeMail.js
+++ Constants/nodeMail.js
@@ -1,97 +1,5 @@
 var nodemailer = require("nodemailer");
-
-// exports.send = function email(emailSendTo, data) {
-//     var transport = nodemailer.createTransport({
-//         host: "smtp.gmail.com",
-//         port: 456,
-//         auth: {
-//           user: process.env.MyEmail,
-//           pass: process.env.PasswordEmail
-//         }
-//       });
-
-//       const random = Math.floor(Math.random() * 9000 + 1000);
-
-//       var mailOptions = {
-//         from: '"Example Team" <from@example.com>',
-//         to: 'user1@example.com, user2@example.com',
-//         subject: 'Nice Nodemailer test',
-//         text: 'Hey there, ${random} it’s our first message sent with Nodemailer ',
-//         html: '<b>Hey there! </b><br> This is our first message sent with Nodemailer<br />${random}/>',
-//         // attachments: [
-//         //   {
-//         //     filename: 'mailtrap.png',
-//         //     path: __dirname + '/mailtrap.png',
-//         //     cid: 'uniq-mailtrap.png' 
-//         //   }
-//         // ]
-//       };
-      
-//       transport.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//           return console.log(error);
-//         }
-//         console.log('Message sent: %s', info.messageId);
-//       });
-// }
-
-
-// module. exports =  function(){
-
-    
-    
-//     async function main() {
-        
-
-//         // Generate test SMTP service account from ethereal.email
-//         // Only needed if you don't have a real mail account for testing
-//         let testAccount = await nodemailer.createTestAccount();
-        
-//         // create reusable transporter object using the default SMTP transport
-//         let transporter = nodemailer.createTransport({
-//             host: "smtp.gmail.com",
-//             port: 465,
-//             secure: true, // true for 465, false for other ports
-//             auth: {
-//                 type: "login",
-//                 user: process.env.MyEmail, // generated ethereal user
-//                 pass: process.env.PasswordEmail, // generated ethereal password
-//             },
-//         });
-        
-//         // send mail with defined transport object
-//         let info = await transporter.sendMail({
-//             from: '"Fred Foo 👻" <foo@example.com>', // sender address
-//             to: "husmitha.silva0@gmail.com", // list of receivers
-//             subject: "Hello ✔", // Subject line
-//             text: "Hello world?", // plain text body
-//             html: "<b>Hello world? <h3><%= random %> </h3></b>", // html body
-//         });
-        
-//         console.log("Message sent: %s", info.messageId);
-//         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-      
-//         // Preview only available when sending through an Ethereal account
-//         console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-//         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-//       }
-
-//       main()
-// }()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//mail settings 
 const MAIL_SETTINGS = {
     service: 'gmail',
     auth: {
@@ -101,9 +9,11 @@ const MAIL_SETTINGS = {
   };
     
   const transporter = nodemailer.createTransport(MAIL_SETTINGS);
-  
+  //sending mail function
   module.exports.sendMail = async (params) => {
     try {
+      //mail body
+      //whom to address includes here
       let info = await transporter.sendMail({
         from: MAIL_SETTINGS.auth.user,
         to: params.to, 
--- Constants/otp.js
+++ Constants/otp.js
@@ -1,5 +1,5 @@
 const otpGenerator = require("otp-generator");
-// const { OTP_LENGTH, OTP_CONFIG } = require('../constants/constants');
+//function for generating otp code
 module.exports.generateOTP = () => {
   let OTP_CONFIG = {
     upperCaseAlphabets: true,
